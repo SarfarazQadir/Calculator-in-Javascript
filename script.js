efdd8d1060eec772ee56b9e7ba6d0f7c4f807108
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-11-09 20:09:26 +0500

    Error Handling with Try Catch

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
 var show = document.getElementById("display");
 var op = document.getElementById("op");
 
+var plus = document.getElementById("plus");
+var minus = document.getElementById("minus");
+var multiply = document.getElementById("multiply");
+var divide = document.getElementById("divide");
+
+
 function appendNumber(param){
   show.value += param;
 }
 
-function appendOperator(param){
+function plusOperator(param){
 //    show.value += param;
-    if(show.value.at(-1) == "+" || show.value.at(-1) == "-" || show.value.at(-1) == "*" || show.value.at(-1) == "/"){
-        var op = document.getElementById("op");
-        op.Disabled;
-  }else{
+    if(show.value.at(-1) == "+"){
+     var b = plus.Disabled;
+   }else{
+    show.value += param;
+  }
+}
+
+function minusOperator(param){
+//    show.value += param;
+    if(show.value.at(-1) == "-"){
+      minus.Disabled;
+   }else{
+    show.value += param;
+  }
+}
+
+function multiplyOperator(param){
+//    show.value += param;
+    if(show.value.at(-1) == "*"){
+      multiply.Disabled;
+   }else{
+    show.value += param;
+  }
+}
+
+function divideOperator(param){
+//    show.value += param;
+    if(show.value.at(-1) == "/"){
+      divide.Disabled;
+   }else{
     show.value += param;
   }
 }
@@ -19,6 +51,10 @@ function clearDisplay(){
 }
 
 function calculate(){
-   var a = show.value = eval(show.value)
+  try {
+    var a = show.value = eval(show.value)    
+} catch (error) {
+  show.value = "Syntax Error";
 }
+  }
  
